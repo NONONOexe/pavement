c5048602ad1f3c480a7d9fb54b1b6125a951b231
Keisuke ANDO <ando@maslab.aitech.ac.jp>
2024-08-26 22:23:06 +0900

    :sparkles: Refactor function to exclude points

--- R/exclude-points.R
+++ R/exclude-points.R
@@ -1,24 +1,40 @@
-#' Exclude points from a points object
+#' Exclude points from a points
 #'
-#' This function exclude points from a points object.
+#' This function excludes points from a points.
 #'
-#' @param points A `sfc` object containing points.
+#' @param input_points A `sfc` object containing points.
 #' @param points_to_exclude A `sfc` object containing points to exclude.
-#' @return A `sfc` object containing points with the excluded points removed.
+#' @return A `sfc` object containing the points from `input_points` that are
+#'   not in `points_to_exclude`.
 #' @export
-exclude_points <- function(points, points_to_exclude) {
-  # Extract the geometry from the `points`
-  points_geometry <- points$geometry
-  # Extract the geometry from the `points_to_exclude`
-  exclude_geometry <- st_cast(points_to_exclude$geometry, "POINT")
+#' @examples
+#' library(sf)
+#'
+#' # Create a set of points and a set of points to exclude
+#' points <- st_sfc(
+#'   st_point(c(0, 0)), st_point(c(1, 0)),
+#'   st_point(c(0, 1)), st_point(c(1, 1))
+#' )
+#' points_to_exclude <- st_sfc(
+#'   st_point(c(0, 0)), st_point(c(1, 1))
+#' )
+#' plot(points)
+#' plot(points_to_exclude, add = TRUE, pch = 4)
+#'
+#' # Exclude points in `points_to_exclude` from `points`
+#' remain_points <- exclude_points(points, points_to_exclude)
+#' plot(remain_points)
+exclude_points <- function(input_points, points_to_exclude) {
+  # Check if each point in `input_points` is equal to any point
+  # in `points_to_exclude`
+  point_match <- st_equals(input_points, points_to_exclude)
 
-  # Check if each point in the `points` is equal
-  # to any point in the `points_to_exclude`
-  is_include <- as.logical(st_equals(points_geometry, exclude_geometry))
+  # Identify points that are not in `points_to_exclude`
+  is_point_to_keep <- sapply(point_match, length) == 0
 
-  # Filter the `points` to keep only the points
-  # that are not in the `points_to_exclude`
-  excluded_points <- points[is.na(is_include), ]
+  # Subset `input_points` to keep only points that are not
+  # in `points_to_exclude`
+  remain_points <- input_points[is_point_to_keep, ]
 
-  return(excluded_points)
+  return(remain_points)
 }
